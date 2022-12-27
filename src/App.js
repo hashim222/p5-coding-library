276a88cf61dfa127e6b17cf5f070096292db41e9
hashim222 <aslamhashim19@gmail.com>
2022-12-27 11:46:45 +0000

    Installed bootstrap in the workspace

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import Button from "react-bootstrap/Button";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello Coders</h1>
+      <Button variant="primary">Primary</Button>
     </div>
   );
 }
