c86ff47298c7e23cc5417d71753cb1dd0df3c819
hashim222 <aslamhashim19@gmail.com>
2022-12-29 13:40:11 +0000

    To make the side navbar, I have added react-bootstrap v5

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import { Container, Row, Col } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import styles from "./App.module.css";
 import SideNavBar from "./components/SideNavBar";
-import ShowNavBar from "./components/ShowNavBar";
 
 function App() {
   return (
     <div className={styles.App}>
       <Container fluid>
         <Row>
-          <Col lg={3} className={`${styles.NavBarBgColor}`}>
-            <ShowNavBar />
+          <Col
+            lg={3}
+            className={`${styles.NavBarBgColor} ${styles.ContextNavBarHeight}`}
+          >
             <SideNavBar />
           </Col>
-          <Col lg={5} style={{backgroundColor:'pink'}}>2</Col>
-          <Col lg={4} style={{backgroundColor:'grey'}}>3</Col>
+          <Col lg={5} style={{ backgroundColor: "pink" }}>
+            2
+          </Col>
+          <Col lg={4} style={{ backgroundColor: "grey" }}>
+            3
+          </Col>
         </Row>
       </Container>
     </div>
--- src/components/SideNavBar.js
+++ src/components/SideNavBar.js
@@ -1,44 +1,80 @@
-import React from "react";
-import { Nav } from "react-bootstrap";
-import styles from "../styles/SideNavBar.module.css";
-import { useGlobalContext } from "./context";
+import { React, useState } from "react";
+import Button from "react-bootstrap/Button";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import Nav from "react-bootstrap/Nav";
+// import styles from '../styles/SideNavBar.module.css'
 
-const SideNavBar = () => {
-  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+function ResponsiveExample() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
-    <div
-      className={`${
-        isSidebarOpen
-          ? `${styles.Sidebar} ${styles.ShowSidebar}`
-          : `${styles.Sidebar}`
-      }`}
-    >
-      <div className={styles.SidebarHeader}>
-        <button className={`${styles.CloseBtn} d-lg-none`} onClick={closeSidebar}>
-          close
-        </button>
+    <div className="mt-3 ml-2">
+      <Button variant="primary" className="d-lg-none" onClick={handleShow}>
+        <i class="fa-solid fa-bars"></i>
+      </Button>
+
+      <div variant="info" className="d-none d-lg-block">
+        <Nav defaultActiveKey="/home" className="flex-column">
+          <div className="fs-3 mt-3">Site Logo</div>
+          <Nav.Link className="mt-5" href="/home">
+            <div>
+              <i class="fa-solid fa-house"></i> Home
+            </div>
+          </Nav.Link>
+          <Nav.Link eventKey="link-1">
+            <div>
+              <i class="fa-solid fa-circle-info"></i> About
+            </div>
+          </Nav.Link>
+          <Nav.Link eventKey="link-2">
+            <div>
+              <i class="fa-solid fa-right-to-bracket"></i> Sign in
+            </div>
+          </Nav.Link>
+          <Nav.Link eventKey="link-3">
+            <div>
+              <i class="fa-solid fa-user-plus"></i> Sign up
+            </div>
+          </Nav.Link>
+        </Nav>
       </div>
-      <div className={styles.SiteLogo}>Logo</div>
-      <Nav
-        defaultActiveKey="/home"
-        className={`${styles.NavLinkHeader} flex-column`}
-      >
-        <Nav.Link href="/home">
-          <div>Home</div>
-        </Nav.Link>
-        <Nav.Link eventKey="link-1">
-          <div>About</div>
-        </Nav.Link>
-        <Nav.Link eventKey="link-2">
-          <div>Sign in</div>
-        </Nav.Link>
-        <Nav.Link eventKey="link-3">
-          <div>Sign up</div>
-        </Nav.Link>
-      </Nav>
+
+      <Offcanvas show={show} onHide={handleClose} responsive="lg">
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Site Logo</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <div className="mb-0 d-lg-none">
+            <Nav defaultActiveKey="/home" className="flex-column">
+              <Nav.Link href="/home">
+                <div>
+                  <i class="fa-solid fa-house"></i>
+                </div>
+              </Nav.Link>
+              <Nav.Link eventKey="link-1">
+                <div>
+                  <i class="fa-solid fa-circle-info"></i>
+                </div>
+              </Nav.Link>
+              <Nav.Link eventKey="link-2">
+                <div>
+                  <i class="fa-solid fa-right-to-bracket"></i>
+                </div>
+              </Nav.Link>
+              <Nav.Link eventKey="link-3">
+                <div>
+                  <i class="fa-solid fa-user-plus"></i>
+                </div>
+              </Nav.Link>
+            </Nav>
+          </div>
+        </Offcanvas.Body>
+      </Offcanvas>
     </div>
   );
-};
+}
 
-export default SideNavBar;
+export default ResponsiveExample;
