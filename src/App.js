007a930731960425b0a56ad7525481e13f5fb744
hashim222 <aslamhashim19@gmail.com>
2022-12-30 16:06:34 +0000

    React router added to navigation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import styles from "./App.module.css";
 import SideNavBar from "./components/SideNavBar";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
@@ -14,7 +15,16 @@ function App() {
           >
             <SideNavBar />
           </Col>
-          <Col lg={5}>Column 2</Col>
+          <Col lg={5}>
+            <Container>
+              <Switch>
+                <Route exact path={"/"} render={() => <h1>Home Page</h1>} />
+                <Route exact path={"/about"} render={() => <h1>About</h1>} />
+                <Route exact path={"/signin"} render={() => <h1>Sign in</h1>} />
+                <Route exact path={"/signup"} render={() => <h1>Sign up</h1>} />
+              </Switch>
+            </Container>
+          </Col>
           <Col lg={4}>Column 3</Col>
         </Row>
       </Container>
--- src/components/SideNavBar.js
+++ src/components/SideNavBar.js
@@ -4,6 +4,7 @@ import Offcanvas from "react-bootstrap/Offcanvas";
 import Nav from "react-bootstrap/Nav";
 import styles from "../styles/SideNavBar.module.css";
 import logo from "../assets/site-logo.png";
+import { NavLink } from "react-router-dom";
 
 function ResponsiveExample() {
   const [show, setShow] = useState(false);
@@ -14,12 +15,14 @@ function ResponsiveExample() {
   return (
     <div className="mt-3">
       <div className={`${styles.NavBarMenuIcon} d-lg-none mt-2 mr-2 mr-md-3`}>
-        <img
-          src={logo}
-          alt="site logo"
-          height="45"
-          className="d-lg-none ml-md-3 ml-2 mt-0.5"
-        />
+        <NavLink to="/">
+          <img
+            src={logo}
+            alt="site logo"
+            height="45"
+            className="d-lg-none ml-md-3 ml-2 mt-0.5"
+          />
+        </NavLink>
         <Button
           style={{ backgroundColor: "#49a6e9" }}
           className="d-lg-none float-end"
@@ -32,68 +35,82 @@ function ResponsiveExample() {
       <div variant="info" className="d-none d-lg-block">
         <Nav defaultActiveKey="/home" className="flex-column">
           <div className="mt-3 ml-3">
-            <img src={logo} alt="site logo" height="75" />
+            <NavLink to="/">
+              <img src={logo} alt="site logo" height="75" />
+            </NavLink>
           </div>
-          <Nav.Link
+          <NavLink
+            exact
+            to="/"
+            activeClassName={styles.ActiveLarge}
             className={`${styles.NavLinkForLargerScreens} mt-5`}
-            href="/home"
           >
             <i class="fa-solid fa-house"></i> Home
-          </Nav.Link>
-          <Nav.Link
-            eventKey="link-1"
+          </NavLink>
+          <NavLink
+            to="/about"
+            activeClassName={styles.ActiveLarge}
             className={styles.NavLinkForLargerScreens}
           >
             <i class="fa-solid fa-circle-info"></i> About
-          </Nav.Link>
-          <Nav.Link
-            eventKey="link-2"
+          </NavLink>
+          <NavLink
+            to="/signin"
+            activeClassName={styles.ActiveLarge}
             className={styles.NavLinkForLargerScreens}
           >
             <i class="fa-solid fa-right-to-bracket"></i> Sign in
-          </Nav.Link>
-          <Nav.Link
-            eventKey="link-3"
+          </NavLink>
+          <NavLink
+            to="signup"
+            activeClassName={styles.ActiveLarge}
             className={styles.NavLinkForLargerScreens}
           >
             <i class="fa-solid fa-user-plus"></i> Sign up
-          </Nav.Link>
+          </NavLink>
         </Nav>
       </div>
 
       <Offcanvas show={show} onHide={handleClose} responsive="lg">
         <Offcanvas.Header closeButton>
           <Offcanvas.Title className="mt-2">
-            <img src={logo} alt="site logo" height="60" />
+            <NavLink to="/">
+              <img src={logo} alt="site logo" height="60" />
+            </NavLink>
           </Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
           <div className="mb-0 d-lg-none">
             <Nav defaultActiveKey="/home" className="flex-column">
-              <Nav.Link
-                href="/home"
+              <NavLink
+                exact
+                to="/"
+                activeClassName={styles.ActiveSmall}
                 className={styles.NavLinksForSmallerScreens}
               >
                 <i class="fa-solid fa-house"></i>
-              </Nav.Link>
-              <Nav.Link
-                eventKey="link-1"
+              </NavLink>
+              <NavLink
+                to="/about"
+                activeClassName={styles.ActiveSmall}
                 className={styles.NavLinksForSmallerScreens}
               >
                 <i class="fa-solid fa-circle-info"></i>
-              </Nav.Link>
-              <Nav.Link
-                eventKey="link-2"
+              </NavLink>
+              <NavLink
+                to="/signin"
+                activeClassName={styles.ActiveSmall}
                 className={styles.NavLinksForSmallerScreens}
               >
                 <i class="fa-solid fa-right-to-bracket"></i>
-              </Nav.Link>
-              <Nav.Link
-                eventKey="link-3"
+              </NavLink>
+              <NavLink
+                to="/signup"
+                activeClassName={styles.ActiveSmall}
                 className={styles.NavLinksForSmallerScreens}
               >
                 <i class="fa-solid fa-user-plus"></i>
-              </Nav.Link>
+              </NavLink>
             </Nav>
           </div>
         </Offcanvas.Body>
