82cc436b7fc7fcbefa298dcf7526a09cfb986c80
hashim222 <aslamhashim19@gmail.com>
2022-12-31 15:11:21 +0000

    To test on the live site, added a slash at the end of /dj-rest-auth/registration

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -28,7 +28,7 @@ const SignUpForm = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      await axios.post("/dj-rest-auth/registration", signUpData);
+      await axios.post("/dj-rest-auth/registration/", signUpData);
       history.push("/signin");
     } catch (error) {
       setErrors(error.response?.data);
