3d623bb19d0959b1761435873edea16a6b63e6ff
hashim222 <aslamhashim19@gmail.com>
2023-01-09 16:18:32 +0000

    Created search bar for the posts

--- src/pages/posts/PostsPageContent.js
+++ src/pages/posts/PostsPageContent.js
@@ -9,16 +9,19 @@ import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
 import noResults from "../../assets/post-search-icon.png";
 import Asset from "../../components/Asset";
+import { Form, Col } from "react-bootstrap";
 
 const PostsPageContent = ({ message, filter = "" }) => {
   const [posts, setPosts] = useState({ results: [] });
   const { pathname } = useLocation();
   const [hasLoaded, setHasLoaded] = useState(false);
 
+  const [query, setQuery] = useState("");
+
   useEffect(() => {
     const fetchPostsFromAPI = async () => {
       try {
-        const { data } = await axiosReq.get(`/posts/?${filter}`);
+        const { data } = await axiosReq.get(`/posts/?${filter}search=${query}`);
         setPosts(data);
         setHasLoaded(true);
       } catch (err) {
@@ -27,11 +30,32 @@ const PostsPageContent = ({ message, filter = "" }) => {
     };
 
     setHasLoaded(false);
-    fetchPostsFromAPI();
-  }, [filter, pathname]);
+    const timer = setTimeout(() => {
+      fetchPostsFromAPI();
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [filter, pathname, query]);
 
   return (
     <Row>
+      <Col>
+        <i className={`fas fa-search ${styles.SearchBarIcon}`} />
+        <Form
+          className={styles.SearchBar}
+          onSubmit={(event) => event.preventDefault()}
+        >
+          <Form.Control
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+            type="text"
+            className="mr-sm-2"
+            placeholder="Search posts"
+          />
+        </Form>
+      </Col>
       {hasLoaded ? (
         <>
           {posts.results.length ? (
@@ -39,13 +63,13 @@ const PostsPageContent = ({ message, filter = "" }) => {
               <Post key={post.id} {...post} setPosts={setPosts} />
             ))
           ) : (
-            <Container className={styles.NoResultsContainer}>
+            <Container className={styles.NoResultsAndSpinnerContainer}>
               <Asset src={noResults} message={message} />
             </Container>
           )}
         </>
       ) : (
-        <Container>
+        <Container className={styles.NoResultsAndSpinnerContainer}>
           <Asset spinner />
         </Container>
       )}
