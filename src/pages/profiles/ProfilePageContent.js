55dedacdf54e24bf3e3b428fe5b3218f292474e8
hashim222 <aslamhashim19@gmail.com>
2023-01-13 18:01:35 +0000

    Profile details were retrieved from the API and displayed

--- src/pages/profiles/ProfilePageContent.js
+++ src/pages/profiles/ProfilePageContent.js
@@ -6,37 +6,98 @@ import Container from "react-bootstrap/Container";
 
 import Asset from "../../components/Asset";
 
+import styles from "../../styles/ProfilePageContent.module.css";
 import appStyles from "../../App.module.css";
+import btnStyles from "../../styles/Button.module.css";
 
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { useParams } from "react-router-dom";
+import {
+  useProfileData,
+  useSetProfileData,
+} from "../../contexts/ProfileDataContext";
+import { axiosReq } from "../../api/axiosDefaults";
+import { Button, Image } from "react-bootstrap";
 
 const ProfilePageContent = () => {
   const [hasLoaded, setHasLoaded] = useState(false);
   const currentUser = useCurrentUser();
+  const { id } = useParams();
+  const { pageProfile } = useProfileData();
+  const setProfileData = useSetProfileData();
+  const [profile] = pageProfile.results;
+  const is_owner = currentUser?.username === profile?.owner;
 
   useEffect(() => {
-    setHasLoaded(true);
-  }, []);
+    const fetchData = async () => {
+      try {
+        const [{ data: pageProfile }] = await Promise.all([
+          axiosReq.get(`/profiles/${id}/`),
+        ]);
+        setProfileData((prevState) => ({
+          ...prevState,
+          pageProfile: { results: [pageProfile] },
+        }));
+        setHasLoaded(true);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, [id, setProfileData]);
 
   const mainProfile = (
     <>
       <Row className="px-3 text-center">
-        <Col lg={3} className="text-lg-left">
-          <p>Image</p>
+        <Col lg={3}>
+          <Image
+            src={profile?.image}
+            className={styles.ProfileImage}
+            roundedCircle
+          />
         </Col>
+
         <Col lg={6}>
-          <h3 className="m-2">Profile username</h3>
-          <p>Profile stats</p>
+          <h3 className="m-2">{profile?.owner}</h3>
+          <Row className="justify-content-center no-gutters">
+            <Col xs={3} className="my-2">
+              <div>{profile?.posts_count}</div>
+              <div>posts</div>
+            </Col>
+            <Col xs={3} className="my-2">
+              <div>{profile?.followers_count}</div>
+              <div>followers</div>
+            </Col>
+            <Col xs={3} className="my-2">
+              <div>{profile?.following_count}</div>
+              <div>followings</div>
+            </Col>
+          </Row>
         </Col>
+
         <Col lg={3} className="text-lg-right">
-          <p>Follow button</p>
+          {currentUser &&
+            !is_owner &&
+            (profile?.following_id ? (
+              <Button onClick={() => {}}>unfollow</Button>
+            ) : (
+              <Button
+                className={`${btnStyles.FormBtns} ${btnStyles.Button} ${btnStyles.BtnHover}`}
+                onClick={() => {}}
+              >
+                follow
+              </Button>
+            ))}
         </Col>
-        <Col className="p-3">
-          <p>Name:</p>
-          <p>Years of experience:</p>
-          <p>Favourite programming language:</p>
-          <p>About me:</p>
-        </Col>
+
+        {profile?.content && (
+          <Col className="p-3">
+            <p>Name:</p>
+            <p>Years of experience:</p>
+            <p>Favourite programming language:</p>
+            <p>About me:</p>
+          </Col>
+        )}
       </Row>
     </>
   );
@@ -50,9 +111,9 @@ const ProfilePageContent = () => {
   );
 
   return (
-    <Row>
+    <Row className={styles.Container}>
       <Col className="py-2 p-0 p-lg-2">
-        <Container>
+        <Container className={appStyles.Content}>
           {hasLoaded ? (
             <>
               {mainProfile}
