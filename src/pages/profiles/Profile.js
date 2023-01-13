742f8fcb24a15d284287896a6edeb4f4ca559f82
hashim222 <aslamhashim19@gmail.com>
2023-01-13 13:49:13 +0000

    A follow and unfollow button has been added

--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
+import btnStyles from "../../styles/Button.module.css";
+
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 const Profile = (props) => {
   const { profile, desktop, mobile, imageSize = 55 } = props;
-  const { id, image, owner } = profile;
+  const { id, following_id, image, owner } = profile;
 
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
 
   return (
     <div
@@ -20,6 +26,21 @@ const Profile = (props) => {
       </div>
       <div>
         <strong>{owner}</strong>
+      </div>
+      <div className={`text-right ${!mobile && "ml-auto"}`}>
+        {!mobile &&
+          currentUser &&
+          !is_owner &&
+          (following_id ? (
+            <Button onClick={() => {}}>unfollow</Button>
+          ) : (
+            <Button
+              className={`${btnStyles.FormBtns} ${btnStyles.Button} ${btnStyles.BtnHover}`}
+              onClick={() => {}}
+            >
+              follow
+            </Button>
+          ))}
       </div>
     </div>
   );
